d737358fadad6007b5f61abb00d9327c7ad609c1
PavanMallela <mallelapavank@gmail.com>
2018-01-01 15:25:59 +0530

    initial commit

---
+++ src/main/java/com/pavanmallela/learning/maveninsteps/MavenInStepsApplication.java
@@ -0,0 +1,12 @@
+package com.pavanmallela.learning.maveninsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenInStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenInStepsApplication.class, args);
+	}
+}
